ee31db1856138edeb94765add502dbcdc67b3383
Nguyễn Quang Thái <45609668+thaiqng@users.noreply.github.com>
2020-07-24 00:54:37 -0500

    Add files via upload
    Add shift register

--- atlixco.ino
+++ atlixco.ino
@@ -1,107 +1,127 @@
-#define IN1 8 
-#define IN2 9 
-#define IN3 10 
-#define IN4 11 
-#define BUTTON 12
+// Shift register 1
+#define shift1_data 4
+#define shift1_latch 5
+#define shift1_clock 6
+byte shift1=0;
 
-int Steps=0; 
-boolean Direction=true;
-int steps_left=0; 
+// Step motor
+#define stepper_in1 0 
+#define stepper_in2 1 
+#define stepper_in3 2 
+#define stepper_in4 3 
+int stepper_step=0; 
+int stepper_steps_left=0; 
+int stepper_delay=1000;
+boolean stepper_direction=true;
+
+// Button
+#define button_in 12
 
 void setup() { 
   Serial.begin(115200); 
-  pinMode(IN1, OUTPUT); 
-  pinMode(IN2, OUTPUT); 
-  pinMode(IN3, OUTPUT); 
-  pinMode(IN4, OUTPUT); 
-  pinMode(BUTTON, INPUT);
+  pinMode(shift1_data, OUTPUT); 
+  pinMode(shift1_latch, OUTPUT); 
+  pinMode(shift1_clock, OUTPUT); 
+  pinMode(button_in, INPUT);
 } 
 
-void loop() { 
-  if (digitalRead(BUTTON)==HIGH) {
-    Direction=!Direction; 
-    steps_left=4095; 
+void loop() {
+  if (digitalRead(button_in) == HIGH) {
+    stepper_direction=!stepper_direction; 
+    stepper_steps_left=4095; 
   }
   
-  while(steps_left>0) { 
-    delayMicroseconds(1000);
-    stepper(); 
-    steps_left--; 
-    Serial.println(steps_left);
+  while(stepper_steps_left>0) { 
+    delayMicroseconds(stepper_delay);
+    stepper_run(); 
+    stepper_steps_left--; 
+    //Serial.println(stepper_steps_left);
   }
 } 
 
-void stepper() { 
-  switch(Steps) { 
+void stepper_run() { 
+  switch(stepper_step) { 
     case 0: 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, HIGH); 
+    bitWrite(shift1, stepper_in1, 0); 
+    bitWrite(shift1, stepper_in2, 0); 
+    bitWrite(shift1, stepper_in3, 0); 
+    bitWrite(shift1, stepper_in4, 1); 
+    write_shift1();
     break; 
     case 1: 
-    digitalWrite(IN1, LOW); 
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, HIGH); 
-    digitalWrite(IN4, HIGH); 
+    bitWrite(shift1, stepper_in1, 0); 
+    bitWrite(shift1, stepper_in2, 0); 
+    bitWrite(shift1, stepper_in3, 1); 
+    bitWrite(shift1, stepper_in4, 1); 
+    write_shift1();
     break; 
     case 2: 
-    digitalWrite(IN1, LOW); 
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, HIGH); 
-    digitalWrite(IN4, LOW); 
+    bitWrite(shift1, stepper_in1, 0); 
+    bitWrite(shift1, stepper_in2, 0); 
+    bitWrite(shift1, stepper_in3, 1); 
+    bitWrite(shift1, stepper_in4, 0); 
+    write_shift1();
     break; 
     case 3: 
-    digitalWrite(IN1, LOW); 
-    digitalWrite(IN2, HIGH); 
-    digitalWrite(IN3, HIGH); 
-    digitalWrite(IN4, LOW); 
+    bitWrite(shift1, stepper_in1, 0); 
+    bitWrite(shift1, stepper_in2, 1); 
+    bitWrite(shift1, stepper_in3, 1); 
+    bitWrite(shift1, stepper_in4, 0); 
+    write_shift1();
     break; 
     case 4: 
-    digitalWrite(IN1, LOW); 
-    digitalWrite(IN2, HIGH); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, LOW); 
+    bitWrite(shift1, stepper_in1, 0); 
+    bitWrite(shift1, stepper_in2, 1); 
+    bitWrite(shift1, stepper_in3, 0); 
+    bitWrite(shift1, stepper_in4, 0); 
+    write_shift1();
     break; 
     case 5: 
-    digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, HIGH); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, LOW); 
+    bitWrite(shift1, stepper_in1, 1); 
+    bitWrite(shift1, stepper_in2, 1); 
+    bitWrite(shift1, stepper_in3, 0); 
+    bitWrite(shift1, stepper_in4, 0); 
+    write_shift1();
     break; 
     case 6: 
-    digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, LOW); 
+    bitWrite(shift1, stepper_in1, 1); 
+    bitWrite(shift1, stepper_in2, 0); 
+    bitWrite(shift1, stepper_in3, 0); 
+    bitWrite(shift1, stepper_in4, 0); 
+    write_shift1();
     break; 
     case 7: 
-    digitalWrite(IN1, HIGH); 
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, HIGH); 
+    bitWrite(shift1, stepper_in1, 1); 
+    bitWrite(shift1, stepper_in2, 0); 
+    bitWrite(shift1, stepper_in3, 0); 
+    bitWrite(shift1, stepper_in4, 1); 
+    write_shift1();
     break; 
     default: 
-    digitalWrite(IN1, LOW); 
-    digitalWrite(IN2, LOW); 
-    digitalWrite(IN3, LOW); 
-    digitalWrite(IN4, LOW); 
+    shift1=0;
+    write_shift1();
     break; 
   } 
-  SetDirection(); 
+  stepper_set_direction(); 
 } 
 
-void SetDirection() { 
-  if(Direction==true) { 
-    Steps++;
+void stepper_set_direction() { 
+  if(stepper_direction==true) { 
+    stepper_step++;
   } 
-  if(Direction==false) { 
-    Steps--; 
+  if(stepper_direction==false) { 
+    stepper_step--; 
   } 
-  if(Steps>7) {
-    Steps=0;
+  if(stepper_step>7) {
+    stepper_step=0;
   } 
-  if(Steps<0) {
-    Steps=7; 
+  if(stepper_step<0) {
+    stepper_step=7; 
   } 
+}
+
+void write_shift1() {
+  digitalWrite(shift1_latch, LOW);
+  shiftOut(shift1_data, shift1_clock, LSBFIRST, shift1);
+  digitalWrite(shift1_latch, HIGH);
 }
